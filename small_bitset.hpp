5401c70c8c141900a5ad57f51930243c1a3d77bc
Muhammed O. Celik <morhancelik@gmail.com>
2024-09-12 22:33:08 -0400

    Created small_bitset class with setter and getter

--- small_bitset.hpp
+++ small_bitset.hpp
@@ -1,9 +1,35 @@
 
 #include <cstddef>
+#include <bitset>
 
 namespace nonstd {
+
     template <std::size_t N>
     class small_bitset {
-        
+
+        using underlying_type_t = std::byte;
+        static constexpr std::size_t underlying_bits() { return 8 * sizeof(underlying_type_t);}
+
+        underlying_type_t data[(N + underlying_bits() - 1) / underlying_bits()];
+
+        constexpr std::size_t indexOf(std::size_t i) const {
+            return (i + underlying_bits() - 1) / underlying_bits();
+        }
+
+    public:
+        constexpr bool operator[](std::size_t i) const {
+            return (data[indexOf(i)] & underlying_type_t(1 << (i%underlying_bits()))) != underlying_type_t(0);
+        }
+
+        small_bitset& set(std::size_t pos, bool value = true) {
+            if (value) {
+                data[indexOf(pos)] |= underlying_type_t(1 << (pos % underlying_bits()));
+            } else {
+                data[indexOf(pos)] &= ~underlying_type_t(1 << (pos % underlying_bits()));
+            }
+            return *this;
+        }
+
+        std::size_t size() const { return N; }
     };
 }
--- test/test.cpp
+++ test/test.cpp
@@ -3,6 +3,21 @@
 
 #include <gtest/gtest.h>
 
-TEST(x,y) {
-    nonstd::small_bitset<8> s;
+TEST(SmallBitset, SingleBit_Set) {
+    nonstd::small_bitset<1> s;
+    s.set(0, true);
+    EXPECT_TRUE(s[0]);
+    s.set(0, false);
+    EXPECT_FALSE(s[0]);
+}
+
+TEST(SmallBitset, ManyBit_Set) {
+    static constexpr std::size_t kNumBits { 128 };
+    nonstd::small_bitset<kNumBits> s;
+    for (int i=0; i < s.size(); i++) {
+        s.set(i, true);
+        EXPECT_TRUE(s[i]);
+        s.set(i, false);
+        EXPECT_FALSE(s[i]);
+    }
 }
