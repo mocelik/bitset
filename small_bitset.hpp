823e63ccda1ce9757362c81665c17cf21b8c93ce
Muhammed O. Celik <morhancelik@gmail.com>
2024-09-12 23:08:00 -0400

    Added formatter

--- small_bitset.hpp
+++ small_bitset.hpp
@@ -1,35 +1,42 @@
 
-#include <cstddef>
 #include <bitset>
+#include <cstddef>
 
 namespace nonstd {
 
-    template <std::size_t N, typename Underlying = std::byte>
-    class small_bitset {
+template <std::size_t N, typename Underlying = std::byte> class small_bitset {
 
-        using underlying_type_t = Underlying;
-        static constexpr std::size_t underlying_bits() { return 8 * sizeof(underlying_type_t);}
+    using underlying_type_t = Underlying;
+    static constexpr std::size_t underlying_bits() {
+        return 8 * sizeof(underlying_type_t);
+    }
 
-        underlying_type_t data[(N + underlying_bits() - 1) / underlying_bits()];
+    underlying_type_t data[(N + underlying_bits() - 1) / underlying_bits()];
 
-        constexpr std::size_t indexOf(std::size_t i) const {
-            return (i + underlying_bits() - 1) / underlying_bits();
+    constexpr std::size_t indexOf(std::size_t i) const {
+        return (i + underlying_bits() - 1) / underlying_bits();
+    }
+
+  public:
+    constexpr bool operator[](std::size_t i) const {
+        return (data[indexOf(i)] &
+                underlying_type_t(1 << (i % underlying_bits()))) !=
+               underlying_type_t(0);
+    }
+
+    small_bitset &set(std::size_t pos, bool value = true) {
+        if (value) {
+            data[indexOf(pos)] |=
+                underlying_type_t(1 << (pos % underlying_bits()));
+        } else {
+            data[indexOf(pos)] &=
+                ~underlying_type_t(1 << (pos % underlying_bits()));
         }
+        return *this;
+    }
 
-    public:
-        constexpr bool operator[](std::size_t i) const {
-            return (data[indexOf(i)] & underlying_type_t(1 << (i%underlying_bits()))) != underlying_type_t(0);
-        }
+    bool test(std::size_t pos) const { return (*this)[pos]; }
 
-        small_bitset& set(std::size_t pos, bool value = true) {
-            if (value) {
-                data[indexOf(pos)] |= underlying_type_t(1 << (pos % underlying_bits()));
-            } else {
-                data[indexOf(pos)] &= ~underlying_type_t(1 << (pos % underlying_bits()));
-            }
-            return *this;
-        }
-
-        std::size_t size() const { return N; }
-    };
-}
+    constexpr std::size_t size() const noexcept { return N; }
+};
+} // namespace nonstd
--- test/test.cpp
+++ test/test.cpp
@@ -14,9 +14,9 @@ TEST(SmallBitset, SingleBit_Set) {
 }
 
 TEST(SmallBitset, ManyBit_Set) {
-    static constexpr std::size_t kNumBits { 128 };
+    static constexpr std::size_t kNumBits{128};
     small_bitset<kNumBits> s;
-    for (int i=0; i < s.size(); i++) {
+    for (int i = 0; i < s.size(); i++) {
         s.set(i, true);
         EXPECT_TRUE(s[i]);
         s.set(i, false);
@@ -25,17 +25,28 @@ TEST(SmallBitset, ManyBit_Set) {
 }
 
 TEST(SmallBitset, UnderlyingType_SizeAdjusted) {
-    EXPECT_GT(  sizeof(small_bitset<1, std::uint64_t>),
-                sizeof(small_bitset<1, std::byte>));
+    EXPECT_GT(sizeof(small_bitset<1, std::uint64_t>),
+              sizeof(small_bitset<1, std::byte>));
 }
 
 TEST(SmallBitset, UnderlyingType_ManyBit_Set) {
-    static constexpr std::size_t kNumBits { 128 };
+    static constexpr std::size_t kNumBits{128};
     small_bitset<kNumBits, std::uint64_t> s;
-    for (int i=0; i < s.size(); i++) {
+    for (int i = 0; i < s.size(); i++) {
         s.set(i, true);
         EXPECT_TRUE(s[i]);
         s.set(i, false);
         EXPECT_FALSE(s[i]);
+    }
+}
+
+TEST(SmallBitset, test) {
+    static constexpr std::size_t kNumBits{128};
+    small_bitset<kNumBits> s;
+    for (int i = 0; i < s.size(); i++) {
+        s.set(i, true);
+        EXPECT_TRUE(s.test(i));
+        s.set(i, false);
+        EXPECT_FALSE(s.test(i));
     }
 }
